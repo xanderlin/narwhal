e603f2f1cd0628a5e25e8d3c98479b67523eca7d
Xander Lin <ajlin@mit.edu>
2014-05-01 17:57:26 -0400

    Initial Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Narwhal::Application.config.secret_key_base = 'e378f3ce92b83c47db0d8ce05229d314f7f18571f8aec226bbc844f25df062e26475238d4beb8abdad66a60626b78c17cd52477dcc7f432e21797b8e4b57dab0'

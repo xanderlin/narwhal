b7b6f644f64395a12c64633711fe7c36ab19ccf1
Xander Lin <ajlin@mit.edu>
2014-05-03 14:53:56 -0400

    ripping out users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,6 @@ require 'securerandom'
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
   # GET /users/1
   # GET /users/1.json
   def show

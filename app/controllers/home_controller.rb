0ccb8ca8462424aba02b396d323dd26c709e2723
Ryan Cheu <ryancheu@mit.edu>
2014-05-14 19:34:12 -0400

    Added randomness to the challenge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index
   	@user = User.new
 
-  	@challenge = "2514315923"
+  	@challenge = SecureRandom.random_number(4074071952668972172536891376818756322102936787331872501272280898708762599526673412366794779).to_s;
   	session[:random_challenge] = @challenge
   end
 end
